fac7773d32abd3bfea635643cbf77d4c78cea8d1
Adro-Ornelas <a22300918@ceti.mx>
2025-06-14 11:40:24 -0600

    MainActivity importada

--- app/src/main/java/com/example/frecuency/MainActivity.java
+++ app/src/main/java/com/example/frecuency/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.frecuency;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    SharedPreferences archivo;
+    //  RecyclerView recyclerView;
+    Button playlist, albums, artistas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +32,77 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        archivo = this.getSharedPreferences("sesion", Context.MODE_PRIVATE);
+        // RecyclerView Para mostrar tablas
+        /*
+        recyclerView = findViewById(R.id.recyclerview);
+        nombrarPlaylist();
+        playlist = findViewById(R.id.btn_playlist);
+        albums = findViewById(R.id.btn_albums);
+        artistas = findViewById(R.id.btn_artistas);*/
+
+    }
+
+    // Inflar options menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Opciones del menu (navega entre activities o logout)
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.opc_principal){
+
+            Toast.makeText(this, "Ya se encuentra aquí.", Toast.LENGTH_SHORT).show();
+        } else if(item.getItemId() == R.id.opc_ver){
+
+            /*
+            Intent aVer = new Intent(this, Ver.class);
+            startActivity(aVer);*/
+/*
+            Intent aVer = new Intent(this, VerCancion.class);
+            startActivity(aVer);
+        } else if(item.getItemId() == R.id.opc_modificar) {
+
+            Intent aMod = new Intent(this, Modificar.class);
+            startActivity(aMod);
+
+        } else if(item.getItemId() == R.id.opc_eliminar) {
+
+            Intent aElim = new Intent(this, Eliminar.class);
+            startActivity(aElim);
+*/
+        } else if(item.getItemId() == R.id.opc_logout) {
+
+            // Si el usuario existe, lo borra de shared preferences y regres a a inicio
+            if(archivo.contains("id_usuario")){
+                Intent cerrar = new Intent(this, Inicio.class);
+                SharedPreferences.Editor editor = archivo.edit();
+                editor.remove("id_usuario");
+                editor.apply();
+                startActivity(cerrar);
+                finish();
+            }
+
+        } else if(item.getItemId() == R.id.opc_creadores) {
+
+            Intent aCrea = new Intent(this, Creadores.class);
+            startActivity(aCrea);
+
+        } else if(item.getItemId() == R.id.opc_contactos) {
+
+            Intent aCont = new Intent(this, Contactos.class);
+            startActivity(aCont);
+
+        } else if (item.getItemId() == R.id.perfil){
+           /* Intent perfil = new Intent(this, Perfil.class);
+            startActivity(perfil);*/
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
